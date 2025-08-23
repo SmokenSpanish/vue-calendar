185b1b450bb4da97374944a023d83a959ba614fd
Maksim <maksimbasov1992@mail.ru>
2025-08-23 14:46:58 +0700

    feat: init Vite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <h1>Calendar</h1>
+    <p>Заглушка</p>
+  </div>
+</template>
+
+
+<script>
+export default { name: 'App' }
+</script>
+
+
+<style>
+body {
+  margin: 0; font-family: system-ui, -apple-system, Segoe UI, Roboto, Arial, sans-serif;
+}
+
+.app {
+  padding: 25px;
+}
+</style>
